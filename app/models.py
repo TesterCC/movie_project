48dfbab62d2f60bff52a0274d013ea4ede59f81c
MFC <liyanxi07@gmail.com>
2018-02-13 23:26:05 +0800

    add: models.py.

--- app/models.py
+++ app/models.py
@@ -20,16 +20,18 @@ class User(db.Model):
     会员
     """
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)   # 编号
+    id = db.Column(db.Integer, primary_key=True)  # 编号
     name = db.Column(db.String(100), unique=True)  # 昵称
-    pwd = db.Column(db.String(100))                # 密码
-    email = db.Column(db.String(100), unique=True)   # 邮箱
-    phone = db.Column(db.String(11), unique=True)    # 手机号码
-    info = db.Column(db.Text)                        # 个性简介
-    face = db.Column(db.String(255), unique=True)    # 头像
+    pwd = db.Column(db.String(100))  # 密码
+    email = db.Column(db.String(100), unique=True)  # 邮箱
+    phone = db.Column(db.String(11), unique=True)  # 手机号码
+    info = db.Column(db.Text)  # 个性简介
+    face = db.Column(db.String(255), unique=True)  # 头像
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 注册时间
-    uuid = db.Column(db.String(255), unique=True)    # 唯一标志符
-    userlogs = db.relationship('Userlog', backref='User')   # 会员日志外键关系关联
+    uuid = db.Column(db.String(255), unique=True)  # 唯一标志符
+    userlogs = db.relationship('Userlog', backref='user')  # 会员日志外键关系关联
+    # backref是反向引用，User和Userlog是一对多的关系，backref是表示在Userlog中新建一个属性user，关联的是Userlog中user_id外键关联的User对象。
+    comments = db.relationship('Comment', backref='user')  # 评论外键关系关联
 
     def __repr__(self):
         return "<User %r>" % self.name
@@ -40,13 +42,81 @@ class Userlog(db.Model):
     会员登录日志
     """
     __tablename__ = "userlog"
-    id = db.Column(db.Integer, primary_key=True)    # 编号
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))   # 所属会员  外键
-    ip = db.Column(db.String(100))    # 登录IP
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)    # 登录时间
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属会员
+    ip = db.Column(db.String(100))  # 登录IP
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 登录时间, UTC(协调世界时)时间
 
     def __repr__(self):
         return "<Userlog %r>" % self.id
 
+
+class Tag(db.Model):
+    """
+    标签
+    """
+    __tablename_ = "tag"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), unique=True)  # 标题
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    movies = db.relationship("Movie", backref="tag")  # 电影外键关系关联
+
+    # backref是反向引用，Movie和Tag是一对多的关系，backref是表示在Movie中新建一个属性tag，关联的是Movie中tag_id外键关联的Tag对象。
+
+    def __repr__(self):
+        return "<Tag %r>" % self.name
+
+
+class Movie(db.Model):
+    __tablename__ = "movie"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    url = db.Column(db.String(255), unique=True)  # 地址
+    info = db.Column(db.Text)  # 简介
+    logo = db.Column(db.String(255), unique=True)  # 封面
+    star = db.Column(db.SmallInteger)  # 星级
+    playnum = db.Column(db.BigInteger)  # 播放量
+    commentnum = db.Column(db.BigInteger)  # 评论量
+    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"))  # 所属标签
+    area = db.Column(db.String(255))  # 上映地区
+    release_time = db.Column(db.Date)  # 上映时间
+    length = db.Column(db.String(100))  # 播放时间
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    comments = db.relationship("Comment", backref="movie")  # 评论外键关系关联
+
+    def __repr__(self):
+        return "<Movie %r>" % self.title
+
+
+class Preview(db.Model):
+    """
+    上映预告
+    """
+    __tablename__ = "preview"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    title = db.Column(db.String(255), unique=True)  # 标题
+    logo = db.Column(db.String(255), unique=True)  # 封面
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+
+class Comment(db.Model):
+    """
+    评论
+    """
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)    # 编号
+    content = db.Column(db.Text)                    # 内容
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))   # 所属电影
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))     # 所属用户
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Comment %r>" % self.id
+
+
+class Moviecol(db.Model):
+    """
+    评论
+    """
 
 
